e3110641f16be6c442311a92245cb9fdee8f1f79
KeoBen <1207257586@qq.com>
2023-07-10 20:47:02 +0800

    master

--- src/main/java/com/keoben/myotsu.java
+++ src/main/java/com/keoben/myotsu.java
@@ -2,9 +2,6 @@ package com.keoben;
 
 public class myotsu {
 	public static void main(String[] args) {
-		System.out.println("myotsu");
-	}
-	public void keoben() {
-		System.out.println("keoben");
+
 	}
 }
